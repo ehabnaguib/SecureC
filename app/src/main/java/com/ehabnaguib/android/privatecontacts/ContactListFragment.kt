a5560a1043cb1b155088559b89fd62178dd89856
ehabnaguib <ehab.naguib.moh@gmail.com>
2023-11-27 02:09:29 +0200

    added action button add contact

--- app/src/main/java/com/ehabnaguib/android/privatecontacts/ContactListFragment.kt
+++ app/src/main/java/com/ehabnaguib/android/privatecontacts/ContactListFragment.kt
@@ -3,6 +3,9 @@ package com.ehabnaguib.android.privatecontacts
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -39,4 +42,22 @@ class ContactListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_contact_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_contact -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
