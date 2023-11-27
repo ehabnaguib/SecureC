71ff53a075e8e4bdbf1c341fd2c3fc2122ea616b
ehabnaguib <ehab.naguib@eng.asu.edu.eg>
2023-11-27 16:24:49 +0200

    added list item contact layout

--- app/src/main/java/com/ehabnaguib/android/privatecontacts/ContactListFragment.kt
+++ app/src/main/java/com/ehabnaguib/android/privatecontacts/ContactListFragment.kt
@@ -8,6 +8,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ehabnaguib.android.privatecontacts.databinding.FragmentContactListBinding
 
@@ -55,6 +56,7 @@ class ContactListFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.new_contact -> {
+                Toast.makeText(requireActivity(), "fuck you", Toast.LENGTH_SHORT).show()
                 true
             }
             else -> super.onOptionsItemSelected(item)
