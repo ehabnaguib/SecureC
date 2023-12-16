7933c2aa5362d086d5265e22f10cf36cf7c0e9d9
ehabnaguib <ehab.naguib.moh@gmail.com>
2023-12-16 15:17:13 +0200

    look up place and update the map accordingly works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,6 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.0")
     implementation("androidx.exifinterface:exifinterface:1.3.6")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.android.libraries.places:places:3.3.0")
     kapt("androidx.room:room-compiler:2.6.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/ehabnaguib/android/privatecontacts/MapFragment.kt
+++ app/src/main/java/com/ehabnaguib/android/privatecontacts/MapFragment.kt
@@ -1,20 +1,32 @@
 package com.ehabnaguib.android.privatecontacts
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.core.os.bundleOf
 import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.navArgs
 import com.ehabnaguib.android.privatecontacts.database.ContactTypeConverters
 import com.ehabnaguib.android.privatecontacts.databinding.FragmentMapBinding
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 
 private const val TAG = "MapFragment"
@@ -31,9 +43,8 @@ class MapFragment : Fragment() {
 
     private var location : LatLng? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var placesClient: PlacesClient
+    private lateinit var predictionsAdapter: ArrayAdapter<AutocompletePrediction>
 
 
     override fun onCreateView(
@@ -41,23 +52,20 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
-
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        val autocompleteFragment = childFragmentManager.findFragmentById(R.id.search_view_fragment)
+                as AutocompleteSupportFragment
 
         location = args.location as LatLng?
-        setFragmentResult(REQUEST_KEY_LOCATION, bundleOf(BUNDLE_KEY_LOCATION to location))
-        mapFragment?.getMapAsync { googleMap ->
-            if (location != null){
-                googleMap.addMarker(MarkerOptions().position(location!!).title("Selected Location"))
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location!!, 10f))
-            }
 
+        mapFragment?.getMapAsync { googleMap ->
             googleMap.setOnMapClickListener { latLng ->
                 googleMap.clear()
                 googleMap.addMarker(MarkerOptions().position(latLng).title("Selected Location"))
@@ -66,11 +74,52 @@ class MapFragment : Fragment() {
             }
         }
 
+        updateMap(mapFragment, location)
+
+        // Getting the API key from the manifest
+        val apiKey = requireContext().applicationContext.packageManager
+            .getApplicationInfo(requireContext().packageName, PackageManager.GET_META_DATA)
+            .metaData.getString("com.google.android.geo.API_KEY")
+
+        // Initialize the Places API
+        if (!Places.isInitialized()) {
+            if (apiKey != null) {
+                Places.initialize(requireContext().applicationContext, apiKey)
+            }
+        }
+        placesClient = Places.createClient(requireContext())
+
+        // Initialize the AutocompleteSupportFragment
 
 
+        // Specify the types of place data to return
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG))
 
+        // Set up a PlaceSelectionListener to handle the response
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                updateMap(mapFragment, place.latLng)
+            }
 
+            override fun onError(status: Status) {
+                // TODO: Handle the error
+            }
+        })
     }
+
+    private fun updateMap(mapFragment: SupportMapFragment?, newLocation : LatLng?) {
+        mapFragment?.getMapAsync { googleMap ->
+            if (newLocation != null) {
+                location = newLocation
+                googleMap.addMarker(MarkerOptions().position(location!!))
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location!!, 14f))
+
+                setFragmentResult(REQUEST_KEY_LOCATION, bundleOf(BUNDLE_KEY_LOCATION to location))
+            }
+        }
+    }
+
+
     companion object {
         const val REQUEST_KEY_LOCATION = "REQUEST_KEY_LOCATION"
         const val BUNDLE_KEY_LOCATION = "BUNDLE_KEY_LOCATION"
